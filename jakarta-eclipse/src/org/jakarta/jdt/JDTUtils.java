ce69aaca989ef513efdde47f32fa7dabc76748bb
Reza Akhavan <akhavanr2015@gmail.com>
2020-10-13 01:49:38 -0700

    Minor edits and fixes to address PR comments
    Signed-off-by: Reza Akhavan <akhavanr2015@gmail.com>

--- jakarta-eclipse/src/org/jakarta/jdt/JDTUtils.java
+++ jakarta-eclipse/src/org/jakarta/jdt/JDTUtils.java
@@ -10,7 +10,10 @@
  * 	Contributors:
  * 		 Red Hat Inc. - initial API and implementation and/or initial documentation
  *******************************************************************************/
+<<<<<<< HEAD
 
+=======
+>>>>>>> Minor edits and fixes to address PR comments
 package org.jakarta.jdt;
 
 import java.io.File;
