e195bdfc342e46140935149942ae0de932a82ad3
Reza Akhavan <39389904+rezaakv@users.noreply.github.com>
2020-10-13 11:24:15 -0700

    Added JDT tools and basic usage on Diagnostic (#17)
    * Added JDT tools pkg and Diagnostics on methods in JakartaLanguageClient
    
    Signed-off-by: Reza Akhavan <akhavanr2015@gmail.com>
    
    * Minor edits and fixes to address PR comments
    
    Signed-off-by: Reza Akhavan <akhavanr2015@gmail.com>
    
    * Edit to exception message in JakartaLanguageClient
    
    Signed-off-by: Reza Akhavan <akhavanr2015@gmail.com>

--- jakarta-eclipse/src/org/jakarta/lsp4e/JakartaLanguageClient.java
+++ jakarta-eclipse/src/org/jakarta/lsp4e/JakartaLanguageClient.java
@@ -1,5 +1,6 @@
 package org.jakarta.lsp4e;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -8,6 +9,11 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.ISourceRange;
 import org.eclipse.lsp4e.LanguageClientImpl;
 import org.eclipse.lsp4j.Diagnostic;
 import org.eclipse.lsp4j.Hover;
@@ -19,60 +25,81 @@ import org.eclipse.lsp4j.Range;
 import org.eclipse.lsp4j.jsonrpc.CancelChecker;
 import org.eclipse.lsp4j.jsonrpc.CompletableFutures;
 import org.eclipse.lsp4j.jsonrpc.messages.Either;
+import org.jakarta.jdt.JDTUtils;
 
 import io.microshed.jakartals.api.JakartaLanguageClientAPI;
 import io.microshed.jakartals.commons.JakartaDiagnosticsParams;
 
 public class JakartaLanguageClient extends LanguageClientImpl implements JakartaLanguageClientAPI {
-    
-    public JakartaLanguageClient() {
-        // do nothing
-    }
-    
-    private IProgressMonitor getProgressMonitor(CancelChecker cancelChecker) {
-        IProgressMonitor monitor = new NullProgressMonitor() {
-            public boolean isCanceled() {
-                cancelChecker.checkCanceled();
-                return false;
-            };
-        };
-        return monitor;
-    }
-    
-    @Override
-    public CompletableFuture<Hover> getJavaHover(HoverParams params) {
-    	// return dummy test hover object
-    	Activator.log(new Status(IStatus.INFO, "hover request received", "hover request received"));
-    	return CompletableFutures.computeAsync((cancelChecker) -> {
-    	    IProgressMonitor monitor = getProgressMonitor(cancelChecker);
-    	    Hover testHover = new Hover();
-    	    List<Either<String, MarkedString>> contents = new ArrayList<>();
-    	    contents.add(Either.forLeft("this is test hover"));
-    	    testHover.setContents(contents);
-    	    return testHover;
-    	 });
-    }
 
-    @Override
-    public CompletableFuture<List<PublishDiagnosticsParams>> getJavaDiagnostics(
-            JakartaDiagnosticsParams javaParams) {
-        Activator.log(new Status(IStatus.INFO, "diagnostic request received", "diagnostic request receieved"));
-        
-        // creating a test diagnostic
-        return CompletableFutures.computeAsync((cancelChecker) -> {
-            IProgressMonitor monitor = getProgressMonitor(cancelChecker);
-                List<PublishDiagnosticsParams> publishDiagnostics = new ArrayList<PublishDiagnosticsParams>();
-                List<String> uris = javaParams.getUris();
-                for (String uri : uris) {
-                    List<Diagnostic> diagnostics = new ArrayList<>();
-                    Range diagRange = new Range(new Position(2, 4), new Position(2, 10));
-                    diagnostics.add(new Diagnostic(diagRange, "testDiagnostic"));
-                    PublishDiagnosticsParams publishDiagnostic = new PublishDiagnosticsParams(uri, diagnostics);
-                    publishDiagnostics.add(publishDiagnostic);
-                }
+	public JakartaLanguageClient() {
+		// do nothing
+	}
 
-                return publishDiagnostics;
-        });
-    }
+	private IProgressMonitor getProgressMonitor(CancelChecker cancelChecker) {
+		IProgressMonitor monitor = new NullProgressMonitor() {
+			public boolean isCanceled() {
+				cancelChecker.checkCanceled();
+				return false;
+			};
+		};
+		return monitor;
+	}
 
+	@Override
+	public CompletableFuture<Hover> getJavaHover(HoverParams params) {
+		// return dummy test hover object
+		Activator.log(new Status(IStatus.INFO, "hover request received", "hover request received"));
+		return CompletableFutures.computeAsync((cancelChecker) -> {
+			IProgressMonitor monitor = getProgressMonitor(cancelChecker);
+			Hover testHover = new Hover();
+			List<Either<String, MarkedString>> contents = new ArrayList<>();
+			contents.add(Either.forLeft("this is test hover"));
+			testHover.setContents(contents);
+			return testHover;
+		});
+	}
+
+	@Override
+	public CompletableFuture<List<PublishDiagnosticsParams>> getJavaDiagnostics(JakartaDiagnosticsParams javaParams) {
+		Activator.log(new Status(IStatus.INFO, "diagnostic request received", "diagnostic request receieved"));
+		// creating a test diagnostic
+		return CompletableFutures.computeAsync((cancelChecker) -> {
+			IProgressMonitor monitor = getProgressMonitor(cancelChecker);
+			List<PublishDiagnosticsParams> publishDiagnostics = new ArrayList<PublishDiagnosticsParams>();
+
+			List<Diagnostic> diagnostics = new ArrayList<>();
+			List<String> uris = javaParams.getUris();
+			for (String uri : uris) {
+
+				URI u = JDTUtils.toURI(uri);
+				ICompilationUnit unit = JDTUtils.resolveCompilationUnit(u);
+				if (unit != null) {
+					// System.out.println("--class name: " + unit.getElementName());
+					IType[] alltypes;
+					try {
+						alltypes = unit.getAllTypes();
+						for (IType type : alltypes) {
+							IMethod[] methods = type.getMethods();
+							for (IMethod method : methods) {
+								// System.out.println("--Method name: " + method.getElementName());
+								// nameRange only has offset and the length of method here
+								ISourceRange nameRange = JDTUtils.getNameRange(method);
+								// System.out.println("--MethodOffset: " + nameRange.getOffset());
+								// System.out.println("--MethodLength: " + nameRange.getLength());
+								Range range = JDTUtils.toRange(unit, nameRange.getOffset(), nameRange.getLength());
+								Range diagRangeMe = range;
+								diagnostics.add(new Diagnostic(diagRangeMe, "A Diagnostic message on every method"));
+							}
+						}
+					} catch (JavaModelException e) {
+						Activator.logException("Cannot calculate diagnostics", e);
+					}
+				}
+				PublishDiagnosticsParams publishDiagnostic = new PublishDiagnosticsParams(uri, diagnostics);
+				publishDiagnostics.add(publishDiagnostic);
+			}
+			return publishDiagnostics;
+		});
+	}
 }
