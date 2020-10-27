9c8452d0acca47f05d3f597a43b0e3b4e8e65ec2
Reza Akhavan <akhavanr2015@gmail.com>
2020-10-27 10:53:51 -0700

    Added @WebFilter and @WebListener annotation diagnostics
    Signed-off-by: Reza Akhavan <akhavanr2015@gmail.com>

--- jakarta-eclipse/src/org/jakarta/jdt/ServletDiagnosticsCollector.java
+++ jakarta-eclipse/src/org/jakarta/jdt/ServletDiagnosticsCollector.java
@@ -29,14 +29,27 @@ public class ServletDiagnosticsCollector implements DiagnosticsCollector{
 				alltypes = unit.getAllTypes();
 				for (IType type : alltypes) {
 					allAnnotations = type.getAnnotations();
-									
+					
+					
 					boolean isWebServletAnnotated = false;
 					boolean isHttpServletExtended = false;
+					
+					boolean isWebFilterAnnotated = false;
+					boolean isFilterImplemented = false;
+					
+					boolean isWebListenerAnnotated = false;
+					boolean isWebListenerInterfaceImplemented = false;
 					
 					for (IAnnotation annotation : allAnnotations) {
 						if (annotation.getElementName().equals(ServletConstants.WEB_SERVLET)) {
 							isWebServletAnnotated = true;
 							break;
+						}
+						if (annotation.getElementName() == "WebFilter") {
+							isWebFilterAnnotated = true;
+						}
+						if (annotation.getElementName() == "WebListener") {
+							isWebListenerAnnotated = true;
 						}
 					}
 
@@ -44,11 +57,61 @@ public class ServletDiagnosticsCollector implements DiagnosticsCollector{
 					if ((typeExtension != null) && typeExtension.equals(ServletConstants.HTTP_SERVLET)) {
 						isHttpServletExtended = true;
 					}
+					
+					String[] implementedInterfaces = type.getSuperInterfaceNames();
+					
+					for(String in: implementedInterfaces) {
+						if (in.compareTo("Filter") == 0) {
+							isFilterImplemented = true;
+						}
+						
+						if (in.compareTo("Filter") == 0) {
+							isFilterImplemented = true;
+						}
+						
+						if (in.compareTo("ServletContextListener") == 0) {
+							isWebListenerInterfaceImplemented = true;
+						}
+						if (in.compareTo("ServletContextAttributeListener") == 0) {
+							isWebListenerInterfaceImplemented = true;
+						}
+						if (in.compareTo("ServletRequestListener") == 0) {
+							isWebListenerInterfaceImplemented = true;
+						}
+						if (in.compareTo("ServletRequestAttributeListener") == 0) {
+							isWebListenerInterfaceImplemented = true;
+						}
+						if (in.compareTo("HttpSessionAttributeListener") == 0) {
+							isWebListenerInterfaceImplemented = true;
+						}
+						if (in.compareTo("HttpSessionAttributeListener") == 0) {
+							isWebListenerInterfaceImplemented = true;
+						}
+						if (in.compareTo("HttpSessionIdListener") == 0) {
+							isWebListenerInterfaceImplemented = true;
+						}
+					}
+
 
 					if (isWebServletAnnotated && !isHttpServletExtended) {
 						ISourceRange nameRange = JDTUtils.getNameRange(type);
 						Range range = JDTUtils.toRange(unit, nameRange.getOffset(), nameRange.getLength());
 						diagnostics.add(new Diagnostic(range, "Classes annotated with @WebServlet must extend the HttpServlet class."));
+					}
+					
+					if (isWebFilterAnnotated && !isFilterImplemented) {
+						ISourceRange nameRange = JDTUtils.getNameRange(type);
+						Range range = JDTUtils.toRange(unit, nameRange.getOffset(), nameRange.getLength());
+						diagnostics.add(new Diagnostic(range, "Classes annotated with @WebFilter must implement the Filter interface."));
+					}
+					
+					if (isWebListenerAnnotated && !isWebListenerInterfaceImplemented) {
+						ISourceRange nameRange = JDTUtils.getNameRange(type);
+						Range range = JDTUtils.toRange(unit, nameRange.getOffset(), nameRange.getLength());
+						diagnostics.add(new Diagnostic(range, "Classes annotated with @WebListener must implement "
+								+ "must implement one or more of the ServletContextListener, ServletContextAttributeListener,"
+								+ " ServletRequestListener, ServletRequestAttributeListener, HttpSessionListener,"
+								+ " HttpSessionAttributeListener, or HttpSessionIdListener interfaces."));
 					}
 					
 				}
