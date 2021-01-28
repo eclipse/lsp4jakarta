ae0e50b6749cc5117c658f62c602c1c803dc8aac
Mandiiiiiii <mandyventurex7@gmail.com>
2021-01-28 19:37:06 +0800

    DeleteAnnotationProposal - Draft
    Signed-off-by: Mandiiiiiii <mandyventurex7@gmail.com>

--- jakarta-eclipse/src/org/jakarta/codeAction/proposal/DeleteAnnotationProposal.java
+++ jakarta-eclipse/src/org/jakarta/codeAction/proposal/DeleteAnnotationProposal.java
@@ -1,9 +1,26 @@
 package org.jakarta.codeAction.proposal;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.Annotation;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.IAnnotationBinding;
 import org.eclipse.jdt.core.dom.IBinding;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
+import org.eclipse.jdt.core.dom.rewrite.ImportRewrite;
+import org.eclipse.jdt.core.dom.rewrite.ImportRewrite.ImportRewriteContext;
+import org.eclipse.jdt.internal.core.manipulation.dom.ASTResolving;
+import org.eclipse.jdt.internal.corext.codemanipulation.ContextSensitiveImportRewriteContext;
+import org.eclipse.lsp4j.CodeActionKind;
+
 
 /**
  * 
@@ -16,9 +33,99 @@ public class DeleteAnnotationProposal extends ChangeCorrectionProposal {
 	 private final IBinding fBinding;
 
 	 private final String[] annotations;
-	 public DeleteAnnotationProposal(String name, String kind, ICompilationUnit cu, ASTRewrite rewrite, int relevance) {
-		 super(name, kind, cu, rewrite, relevance);
-		 // TODO Auto-generated constructor stub
+	 public DeleteAnnotationProposal(String label, ICompilationUnit targetCU, CompilationUnit invocationNode,
+	            IBinding binding, int relevance, String... annotations) {
+		 super(label, CodeActionKind.QuickFix, targetCU, null, relevance);
+	     fInvocationNode = invocationNode;
+	     fBinding = binding;
+	     this.annotations = annotations;
 	 }
 	
+	 @Override
+	 protected ASTRewrite getRewrite() throws CoreException {
+		 ASTNode declNode = null;
+	     ASTNode boundNode = fInvocationNode.findDeclaringNode(fBinding);
+	     CompilationUnit newRoot = fInvocationNode;
+	     if (boundNode != null) {
+	    	 declNode = boundNode; // is same CU
+	     } else {
+	    	 newRoot = ASTResolving.createQuickFixAST(getCompilationUnit(), null);
+	    	 declNode = newRoot.findDeclaringNode(fBinding.getKey());
+	     }
+	     ImportRewrite imports = createImportRewrite(newRoot);
+	     
+	     boolean isField = declNode instanceof VariableDeclarationFragment;
+	     if (isField) {
+	    	 declNode = declNode.getParent();
+	     }
+	     
+	     String[] annotations = getAnnotations();
+
+	     // get short name of annotations
+	     String[] annotationShortNames = new String[annotations.length];
+	     for (int i = 0; i < annotations.length; i++) {
+	    	 String shortName = annotations[i].substring(annotations[i].lastIndexOf(".") + 1, annotations[i].length());
+	         annotationShortNames[i] = shortName;
+	     }
+	     
+	     if (declNode instanceof TypeDeclaration || isField) {
+	    	 AST ast = declNode.getAST();
+	    	 ASTRewrite rewrite = ASTRewrite.create(ast);
+	    	 
+	    	 ImportRewriteContext importRewriteContext = new ContextSensitiveImportRewriteContext(declNode, imports);
+	    	 
+	    	 // remove annotations in the removeAnnotations list
+	    	 @SuppressWarnings("unchecked")
+	    	 List<? extends ASTNode> children = (List<? extends ASTNode>) declNode
+	    	 	.getStructuralProperty(TypeDeclaration.MODIFIERS2_PROPERTY);
+	         Annotation existingAnnotation = null;
+
+	            // find and save existing annotation, then remove it from ast
+	         for (ASTNode child : children) {
+	        	 if (child instanceof Annotation) {
+	        		 Annotation annotation = (Annotation) child;
+	        		 IAnnotationBinding annotationBinding = annotation.resolveAnnotationBinding();
+
+	        		 boolean containsAnnotation = Arrays.stream(annotationShortNames)
+	        				 .anyMatch(annotation.getTypeName().toString()::contains);
+	        		 if (containsAnnotation) {
+	        			 existingAnnotation = annotation;
+	        			 rewrite.remove(child, null);
+	        		 }
+	        		 rewrite.remove(child, null);
+	        	 }
+	         }
+	         
+	         return rewrite;
+	     }
+	     
+	     return null;
+	 }
+	 
+	 /**
+	  * Returns the Compilation Unit node
+	  *
+	  * @return the invocation node for the Compilation Unit
+	  */
+	  protected CompilationUnit getInvocationNode() {
+		  return this.fInvocationNode;
+	  }
+
+	  /**
+	   * Returns the Binding object associated with the new annotation change
+	   * 
+	   * @return the binding object
+	   */
+	   protected IBinding getBinding() {
+		   return this.fBinding;
+	   }
+
+	  /**
+	   * Returns the annotations list
+	   * 
+	   * @return the list of new annotations to add
+	   */
+	   protected String[] getAnnotations() {
+		   return this.annotations;
+	   }
 }
