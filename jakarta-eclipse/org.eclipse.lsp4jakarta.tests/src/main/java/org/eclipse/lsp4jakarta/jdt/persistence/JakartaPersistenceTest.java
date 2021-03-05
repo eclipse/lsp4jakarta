0f5a278092542af00b165dc5163d95d58c5604e5
lamminade <leslie.dawson@mail.mcgill.ca>
2021-03-05 13:17:00 -0500

    Add tests for removing final modifiers
    Signed-off-by: lamminade <leslie.dawson@mail.mcgill.ca>

--- jakarta-eclipse/org.eclipse.lsp4jakarta.tests/src/main/java/org/eclipse/lsp4jakarta/jdt/persistence/JakartaPersistenceTest.java
+++ jakarta-eclipse/org.eclipse.lsp4jakarta.tests/src/main/java/org/eclipse/lsp4jakarta/jdt/persistence/JakartaPersistenceTest.java
@@ -156,4 +156,71 @@ public class JakartaPersistenceTest extends BaseJakartaTest {
 
         assertJavaCodeAction(codeActionParams1, utils, ca1, ca2);
     }
+
+    @Test
+    public void removeFinalModifiers() throws Exception {
+        JDTUtils utils = JDT_UTILS;
+
+        IJavaProject javaProject = loadJavaProject("jakarta-sample", "");
+        IFile javaFile = javaProject.getProject().getFile(
+                new Path("src/main/java/io/openliberty/sample/jakarta/persistence/FinalModifiers.java"));
+        String uri = javaFile.getLocation().toFile().toURI().toString();
+
+        JakartaDiagnosticsParams diagnosticsParams = new JakartaDiagnosticsParams();
+        diagnosticsParams.setUris(Arrays.asList(uri));
+
+        // test diagnostics are present
+        Diagnostic d1 = d(10, 21, 28,
+                "A class using the @Entity annotation cannot contain any methods that are declared final",
+                DiagnosticSeverity.Error, "jakarta-persistence", "RemoveFinalMethods");
+
+        Diagnostic d2 = d(7, 14, 15,
+                "A class using the @Entity annotation cannot contain any persistent instance variables that are declared final",
+                DiagnosticSeverity.Error, "jakarta-persistence", "RemoveFinalVariables");
+
+        Diagnostic d3 = d(8, 17, 18,
+                "A class using the @Entity annotation cannot contain any persistent instance variables that are declared final",
+                DiagnosticSeverity.Error, "jakarta-persistence", "RemoveFinalVariables");
+
+        Diagnostic d4 = d(8, 30, 31,
+                "A class using the @Entity annotation cannot contain any persistent instance variables that are declared final",
+                DiagnosticSeverity.Error, "jakarta-persistence", "RemoveFinalVariables");
+
+        Diagnostic d5 = d(5, 19, 29,
+                "A class using the @Entity annotation must not be final.",
+                DiagnosticSeverity.Error, "jakarta-persistence", "InvalidClass");
+
+        assertJavaDiagnostics(diagnosticsParams, utils, d1, d2, d3, d4, d5);
+
+        // test quick fixes
+        JakartaJavaCodeActionParams codeActionParams1 = createCodeActionParams(uri, d1);
+        TextEdit te1 = te(10, 10, 10, 16, "");
+        CodeAction ca1 = ca(uri,  "Remove the 'final' modifier from this method", d1, te1);
+
+        assertJavaCodeAction(codeActionParams1, utils, ca1);
+
+        JakartaJavaCodeActionParams codeActionParams2 = createCodeActionParams(uri, d2);
+        TextEdit te2 = te(7, 4, 7, 10, "");
+        CodeAction ca2 = ca(uri,  "Remove the 'final' modifier from this variable", d2, te2);
+
+        assertJavaCodeAction(codeActionParams2, utils, ca2);
+
+        JakartaJavaCodeActionParams codeActionParams3 = createCodeActionParams(uri, d3);
+        TextEdit te3 = te(8, 4, 8, 10, "");
+        CodeAction ca3 = ca(uri,  "Remove the 'final' modifier from this variable", d3, te3);
+
+        assertJavaCodeAction(codeActionParams3, utils, ca3);
+
+        JakartaJavaCodeActionParams codeActionParams4 = createCodeActionParams(uri, d4);
+        TextEdit te4 = te(8, 4, 8, 10, "");
+        CodeAction ca4 = ca(uri,  "Remove the 'final' modifier from this variable", d4, te4);
+
+        assertJavaCodeAction(codeActionParams4, utils, ca4);
+
+        JakartaJavaCodeActionParams codeActionParams5 = createCodeActionParams(uri, d5);
+        TextEdit te5 = te(5, 6, 5, 12, "");
+        CodeAction ca5 = ca(uri, "Remove the 'final' modifier from this class", d5, te5);
+
+        assertJavaCodeAction(codeActionParams5, utils, ca5);
+    }
 }
