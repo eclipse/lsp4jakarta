8381f6e73d64b455fb12c838017fe5c9a3013397
MShocrylas <shocryla@ualberta.ca>
2021-04-08 16:25:35 -0600

    Forgot to commit second test file in last commit
    Signed-off-by: MShocrylas <shocryla@ualberta.ca>

---
+++ jakarta-eclipse/org.eclipse.lsp4jakarta.tests/projects/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/jax_rs/RootResourceClassConstructorsEqualLen.java
@@ -0,0 +1,15 @@
+package io.openliberty.sample.jakarta.jax_rs;
+
+import jakarta.ws.rs.Path;
+
+@Path("/somewhere")
+public class RootResourceClassConstructorsEqualLen {
+	
+	public RootResourceClassConstructorsEqualLen(int arg1) {
+		
+	}
+	
+	public RootResourceClassConstructorsEqualLen(String arg1) {
+		
+	}
+}
