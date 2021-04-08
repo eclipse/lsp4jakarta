97a205da6ea2011e8e66e71eef750a750a026067
MShocrylas <shocryla@ualberta.ca>
2021-04-08 01:15:42 -0600

    Added tests file, automated tests not implemented yet
    Signed-off-by: MShocrylas <shocryla@ualberta.ca>

---
+++ jakarta-eclipse/org.eclipse.lsp4jakarta.tests/projects/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/jax_rs/RootResourceClassConstructors.java
@@ -0,0 +1,19 @@
+package io.openliberty.sample.jakarta.jax_rs;
+
+import jakarta.ws.rs.Path;
+
+@Path("/somewhere")
+public class RootResourceClassConstructors {
+
+	public RootResourceClassConstructors() {
+		
+	}
+	
+	public RootResourceClassConstructors(int arg1) {
+		
+	}
+	
+	public RootResourceClassConstructors(String arg1) {
+		
+	}
+}
