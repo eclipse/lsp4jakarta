b9ba7b60e1bd2e6d7537b5e8befab577167309ea
Himanshu Chotwani <himanshuc71@gmail.com>
2021-10-10 13:28:00 -0700

    Added DiagnosticCollector for Dependency Injection

---
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/core/di/DependencyInjectionDiagnosticsCollector.java
@@ -0,0 +1,70 @@
+package org.eclipse.lsp4jakarta.jdt.core.di;
+
+import static org.eclipse.lsp4jakarta.jdt.core.di.DependencyInjectionConstants.DIAGNOSTIC_SOURCE;
+import static org.eclipse.lsp4jakarta.jdt.core.di.DependencyInjectionConstants.SEVERITY;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.jdt.core.Flags;
+import org.eclipse.jdt.core.IAnnotation;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IField;
+import org.eclipse.jdt.core.ISourceRange;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.lsp4j.Diagnostic;
+import org.eclipse.lsp4j.Range;
+import org.eclipse.lsp4jakarta.jdt.core.DiagnosticsCollector;
+import org.eclipse.lsp4jakarta.jdt.core.JDTUtils;
+import org.eclipse.lsp4jakarta.jdt.core.JakartaCorePlugin;
+
+public class DependencyInjectionDiagnosticsCollector implements DiagnosticsCollector{
+
+	@Override
+	public void completeDiagnostic(Diagnostic diagnostic) {
+		diagnostic.setSource(DIAGNOSTIC_SOURCE);
+        diagnostic.setSeverity(SEVERITY);
+		
+	}
+
+	@Override
+	public void collectDiagnostics(ICompilationUnit unit, List<Diagnostic> diagnostics) {
+		if (unit == null)
+            return;
+		Diagnostic diagnostic;
+		IType[] alltypes;
+        IAnnotation[] allAnnotations;
+		try {
+			alltypes = unit.getAllTypes();
+			for (IType type : alltypes) {
+				allAnnotations = type.getAnnotations();
+				
+				IField[] allFields = type.getFields();
+				for (IField field : allFields) {
+					int fieldFlags = field.getFlags();
+					List<IAnnotation> fieldAnnotations = Arrays.asList(field.getAnnotations());
+					
+					boolean isInjectField = fieldAnnotations.stream()
+                            .anyMatch(annotation -> annotation.getElementName()
+                            		.equals(DependencyInjectionConstants.INJECT));
+					
+					boolean isFinal = Flags.isFinal(fieldFlags);
+					
+					if (isFinal && isInjectField) {
+						ISourceRange nameRange = JDTUtils.getNameRange(field);
+						Range range = JDTUtils.toRange(unit, nameRange.getOffset(), nameRange.getLength());
+						String msg = "Injectable fields are not final";
+						diagnostic = new Diagnostic(range, msg);
+						diagnostic.setCode(DependencyInjectionConstants.DIAGNOSTIC_CODE_INJECT_FINAL);
+						completeDiagnostic(diagnostic);
+						diagnostics.add(diagnostic);
+					}
+				}
+			}
+		} catch (JavaModelException e) {
+        	JakartaCorePlugin.logException("Cannot calculate diagnostics", e);
+        }
+	}
+
+}
