d51fc04155cc19808ccf1932ef363ad68c0e351a
Himanshu Chotwani <himanshuc71@gmail.com>
2021-11-18 16:35:08 -0800

    Refactor for PersistenceEntityQuickFix.java
    Moved generic quickFixes under codeActions from DependencyInjection
    refactored PersistenceEntityQuickFix.java to utilize RemoveFinalModifierQuickFix.java

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/codeAction/proposal/quickfix/RemoveInjectAnnotationQuickFix.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/codeAction/proposal/quickfix/RemoveInjectAnnotationQuickFix.java
@@ -11,22 +11,7 @@
 *     IBM Corporation, Himanshu Chotwani - initial API and implementation
 *******************************************************************************/
 
-package org.eclipse.lsp4jakarta.jdt.core.di;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.jdt.core.dom.ASTNode;
-import org.eclipse.jdt.core.dom.IBinding;
-import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
-import org.eclipse.jdt.internal.corext.dom.Bindings;
-import org.eclipse.lsp4j.CodeAction;
-import org.eclipse.lsp4j.Diagnostic;
-import org.eclipse.lsp4jakarta.jdt.codeAction.IJavaCodeActionParticipant;
-import org.eclipse.lsp4jakarta.jdt.codeAction.JavaCodeActionContext;
-import org.eclipse.lsp4jakarta.jdt.codeAction.proposal.ModifyModifiersProposal;
-import org.eclipse.lsp4jakarta.jdt.codeAction.proposal.quickfix.RemoveAnnotationConflictQuickFix;
+package org.eclipse.lsp4jakarta.jdt.codeAction.proposal.quickfix;
 
 /**
  * 
