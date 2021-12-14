839d6aeda454337f59fd2fad853607480c4c5bc0
Zijian Pei <80144653+Future-Genius@users.noreply.github.com>
2021-12-14 10:07:28 -0500

    219 test  (#229)
    * Add test cases
    
    * Add test for PreDestroy QuickFix
    
    * Complete PreDestroy QuickFix Test
    
    * Complete test for @Resource QuickFix
    
    * Test for @PostConstruct
    
    * Finish Test for @PostConstruct
    
    * Change format
    
    * add header

---
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/src/main/java/org/eclipse/lsp4jakarta/jdt/annotations/PostConstructAnnotationTest.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (c) 2021 IBM Corporation and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.lsp4jakarta.jdt.annotations;
+
+import static org.eclipse.lsp4jakarta.jdt.core.JakartaForJavaAssert.assertJavaCodeAction;
+import static org.eclipse.lsp4jakarta.jdt.core.JakartaForJavaAssert.assertJavaDiagnostics;
+import static org.eclipse.lsp4jakarta.jdt.core.JakartaForJavaAssert.ca;
+import static org.eclipse.lsp4jakarta.jdt.core.JakartaForJavaAssert.createCodeActionParams;
+import static org.eclipse.lsp4jakarta.jdt.core.JakartaForJavaAssert.d;
+import static org.eclipse.lsp4jakarta.jdt.core.JakartaForJavaAssert.te;
+
+import java.util.Arrays;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.lsp4j.CodeAction;
+import org.eclipse.lsp4j.Diagnostic;
+import org.eclipse.lsp4j.DiagnosticSeverity;
+import org.eclipse.lsp4j.TextEdit;
+import org.eclipse.lsp4jakarta.commons.JakartaDiagnosticsParams;
+import org.eclipse.lsp4jakarta.commons.JakartaJavaCodeActionParams;
+import org.eclipse.lsp4jakarta.jdt.core.BaseJakartaTest;
+import org.eclipse.lsp4jakarta.jdt.core.JDTUtils;
+import org.junit.Test;
+
+public class PostConstructAnnotationTest extends BaseJakartaTest {
+
+	protected static JDTUtils JDT_UTILS = new JDTUtils();
+
+	@Test
+	public void GeneratedAnnotation() throws Exception {
+		IJavaProject javaProject = loadJavaProject("jakarta-sample", "");
+		IFile javaFile = javaProject.getProject().getFile(
+				new Path("src/main/java/io/openliberty/sample/jakarta/annotations/PostConstructAnnotation.java"));
+		String uri = javaFile.getLocation().toFile().toURI().toString();
+
+		JakartaDiagnosticsParams diagnosticsParams = new JakartaDiagnosticsParams();
+		diagnosticsParams.setUris(Arrays.asList(uri));
+
+		// expected Diagnostics
+
+		Diagnostic d1 = d(15, 16, 28, "A method with the annotation @PostConstruct must be void.",
+				DiagnosticSeverity.Error, "jakarta-annotations", "PostConstructReturnType");
+
+		Diagnostic d2 = d(20, 13, 25, "A method with the annotation @PostConstruct should not have any parameters.",
+				DiagnosticSeverity.Error, "jakarta-annotations", "PostConstructParams");
+
+		Diagnostic d3 = d(25, 13, 25, "A method with the annotation @PostConstruct must not throw checked exceptions.",
+				DiagnosticSeverity.Warning, "jakarta-annotations", "PostConstructException");
+
+		assertJavaDiagnostics(diagnosticsParams, JDT_UTILS, d1, d2, d3);
+
+		JakartaJavaCodeActionParams codeActionParams = createCodeActionParams(uri, d2);
+		TextEdit te = te(19, 1, 20, 1, "");
+		TextEdit te1 = te(20, 26, 20, 37, "");
+		CodeAction ca = ca(uri, "Remove @PostConstruct", d2, te);
+		CodeAction ca1 = ca(uri, "Remove all parameters", d2, te1);
+		assertJavaCodeAction(codeActionParams, JDT_UTILS, ca, ca1);
+
+	}
+
+}
--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/src/main/java/org/eclipse/lsp4jakarta/jdt/annotations/PreDestroyAnnotationTest.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/src/main/java/org/eclipse/lsp4jakarta/jdt/annotations/PreDestroyAnnotationTest.java
@@ -19,9 +19,12 @@ import java.util.Arrays;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.lsp4j.CodeAction;
 import org.eclipse.lsp4j.Diagnostic;
 import org.eclipse.lsp4j.DiagnosticSeverity;
+import org.eclipse.lsp4j.TextEdit;
 import org.eclipse.lsp4jakarta.commons.JakartaDiagnosticsParams;
+import org.eclipse.lsp4jakarta.commons.JakartaJavaCodeActionParams;
 import org.eclipse.lsp4jakarta.jdt.core.BaseJakartaTest;
 import org.eclipse.lsp4jakarta.jdt.core.JDTUtils;
 import org.junit.Test;
@@ -53,6 +56,21 @@ public class PreDestroyAnnotationTest extends BaseJakartaTest {
                 DiagnosticSeverity.Warning, "jakarta-annotations", "PreDestroyException");
 
         assertJavaDiagnostics(diagnosticsParams, JDT_UTILS, d2, d1, d3);
+        
+        
+        JakartaJavaCodeActionParams codeActionParams = createCodeActionParams(uri, d1);
+        TextEdit te = te(19, 1, 20, 1,"");
+        TextEdit te1 = te(20, 29, 20, 40,"");
+        CodeAction ca = ca(uri, "Remove @PreDestroy", d1, te);
+        CodeAction ca1= ca(uri, "Remove all parameters", d1, te1);
+        assertJavaCodeAction(codeActionParams, JDT_UTILS, ca, ca1);
+        
+        JakartaJavaCodeActionParams codeActionParams1 = createCodeActionParams(uri, d2);
+        TextEdit te2 = te(25, 1, 26, 1,"");
+        TextEdit te3 = te(26, 7, 26, 14,"");
+        CodeAction ca2 = ca(uri, "Remove @PreDestroy", d2, te2);
+        CodeAction ca3= ca(uri, "Remove the 'static' modifier from this method", d2, te3);
+        assertJavaCodeAction(codeActionParams1, JDT_UTILS, ca2, ca3);
 
     }
 
