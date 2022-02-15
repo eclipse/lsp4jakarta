d9997aa71050886517f10f3eb187f346b0a19a2a
Lidia Ataupillco <ataupill@ualberta.ca>
2022-02-15 14:34:57 -0700

    added new order

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/core/websockets/WebSocketDiagnosticsCollector.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/core/websockets/WebSocketDiagnosticsCollector.java
@@ -2,12 +2,15 @@ package org.eclipse.lsp4jakarta.jdt.core.websockets;
 
 import java.util.List;
 
+import org.eclipse.jdt.core.IAnnotation;
 import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IField;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.lsp4j.Diagnostic;
 import org.eclipse.lsp4jakarta.jdt.core.DiagnosticsCollector;
-import org.eclipse.jdt.core.IAnnotation;
-import org.eclipse.jdt.core.IType;
-import org.eclipse.jdt.core.IMethod;
+import org.eclipse.lsp4jakarta.jdt.core.JakartaCorePlugin;
 
 
 
@@ -39,7 +42,11 @@ public class WebSocketDiagnosticsCollector implements DiagnosticsCollector {
 	}
 
 	private void invalidParamsCheck(ICompilationUnit unit, List <Diagnostic> diagnostics, IType type) {
-		
+		// check methods annotations, then check their params, check that their type is any of the valid options, if it's a string, check that it has the @Params annotation
+		// If so, set up the severity to Error, and add a code?
 	}
 	
+	private boolean isWebSocketClass() {
+		return false;
+	}
 }
