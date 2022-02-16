7d141120d8618009b5bfb422aefaf8c588c5cb07
Lana Kang <sullan.kang@mail.mcgill.ca>
2022-02-16 14:42:31 -0500

    Add Transactions Package(constants, diagnostics collector, snippet json)

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/core/JDTServicesManager.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/core/JDTServicesManager.java
@@ -41,6 +41,7 @@ import org.eclipse.lsp4jakarta.jdt.core.persistence.PersistenceMapKeyDiagnostics
 import org.eclipse.lsp4jakarta.jdt.core.servlet.FilterDiagnosticsCollector;
 import org.eclipse.lsp4jakarta.jdt.core.servlet.ListenerDiagnosticsCollector;
 import org.eclipse.lsp4jakarta.jdt.core.servlet.ServletDiagnosticsCollector;
+import org.eclipse.lsp4jakarta.jdt.core.transactions.TransactionsDiagnosticsCollector;
 import org.eclipse.lsp4jakarta.jdt.core.JakartaCorePlugin;
 
 import org.eclipse.lsp4jakarta.jdt.codeAction.CodeActionHandler;
@@ -77,6 +78,7 @@ public class JDTServicesManager {
         diagnosticsCollectors.add(new JsonbCreatorDiagnosticsCollector());
         diagnosticsCollectors.add(new ManagedBeanDiagnosticsCollector());
         diagnosticsCollectors.add(new DependencyInjectionDiagnosticsCollector());
+        diagnosticsCollectors.add(new TransactionsDiagnosticsCollector());
         this.codeActionHandler = new CodeActionHandler();
     }
 
