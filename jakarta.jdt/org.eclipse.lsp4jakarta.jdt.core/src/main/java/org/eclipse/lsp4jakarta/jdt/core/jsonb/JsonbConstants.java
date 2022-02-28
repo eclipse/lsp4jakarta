ea2ad486142943719c97285260a0fa28bc3942a9
Adit Rada <rada_adit@hotmail.com>
2022-02-28 12:32:42 -0700

    fixed indentation issues

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/core/jsonb/JsonbConstants.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/core/jsonb/JsonbConstants.java
@@ -2,20 +2,20 @@ package org.eclipse.lsp4jakarta.jdt.core.jsonb;
 
 public class JsonbConstants {
 
-	/* Source */
-	public static final String DIAGNOSTIC_SOURCE = "jakarta-jsonb";
+    /* Source */
+    public static final String DIAGNOSTIC_SOURCE = "jakarta-jsonb";
 
-	/* Code */
-	public static final String DIAGNOSTIC_CODE_ANNOTATION = "MultipleJsonbCreatorAnnotations";
+    /* Code */
+    public static final String DIAGNOSTIC_CODE_ANNOTATION = "MultipleJsonbCreatorAnnotations";
 
-	/* Annotation Constants */
-	public static final String JSONB_PREFIX = "Jsonb";
+    /* Annotation Constants */
+    public static final String JSONB_PREFIX = "Jsonb";
 
-	public static final String JSONB_CREATOR = JSONB_PREFIX + "Creator";
-	public static final String ERROR_MESSAGE_JSONB_CREATOR = "Only one constructor or static factory method can be annotated with @JsonbCreator in a given class.";
-	public static final int MAX_METHOD_WITH_JSONBCREATOR = 1;
+    public static final String JSONB_CREATOR = JSONB_PREFIX + "Creator";
+    public static final String ERROR_MESSAGE_JSONB_CREATOR = "Only one constructor or static factory method can be annotated with @JsonbCreator in a given class.";
+    public static final int MAX_METHOD_WITH_JSONBCREATOR = 1;
 
-	public static final String JSONB_TRANSIENT = JSONB_PREFIX + "Transient";
-	public static final String ERROR_MESSAGE_JSONB_TRANSIENT = "@JsonbTransient should be mutually exclusive with all other JSON Binding defined annotations.";
+    public static final String JSONB_TRANSIENT = JSONB_PREFIX + "Transient";
+    public static final String ERROR_MESSAGE_JSONB_TRANSIENT = "@JsonbTransient should be mutually exclusive with all other JSON Binding defined annotations.";
 
 }
