2232a10b047eb6af2c580ef73e0a8fcee34b56b0
Aviral Saxena <aviral1231@gmail.com>
2022-04-11 23:32:04 -0700

    Add Imports to Test

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/projects/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/websocket/InvalidParamTypePong.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/projects/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/websocket/InvalidParamTypePong.java
@@ -1,20 +1,24 @@
 package io.openliberty.sample.jakarta.websocket;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.nio.ByteBuffer;
 
-import jakarta.websocket.CloseReason;
-import jakarta.websocket.OnOpen;
-import jakarta.websocket.OnClose;
+import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.PongMessage;
+import jakarta.websocket.OnError;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.EndpointConfig;
+import jakarta.websocket.OnClose;
+import jakarta.websocket.Session;
 
-/**
- * Expected Diagnostics are related to validating that the parameters for the 
- * WebSocket methods are of valid types for onOpen (diagnostic code: OnOpenChangeInvalidParam) 
- * and onClose (diagnostic code: OnCloseChangeInvalidParam).
- * See issues #247 (onOpen) and #248 (onClose)
- */
-@ServerEndpoint(value = "/infos")
+@ServerEndpoint(value = "/demo/{test}/var/{abcd}")
 public class InvalidParamTypePong {
+    private static Session session; 
+
     @OnOpen
     public void OnOpen(Session session) throws IOException {
         System.out.println("Websocket opened: " + session.getId().toString());
--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/projects/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/websocket/InvalidParamTypeText.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.test/projects/jakarta-sample/src/main/java/io/openliberty/sample/jakarta/websocket/InvalidParamTypeText.java
@@ -1,19 +1,21 @@
 package src.main.java.io.openliberty.sample.jakarta.websocket;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.nio.ByteBuffer;
 
-import jakarta.websocket.CloseReason;
-import jakarta.websocket.OnOpen;
-import jakarta.websocket.OnClose;
+import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.PongMessage;
+import jakarta.websocket.OnError;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.EndpointConfig;
+import jakarta.websocket.OnClose;
+import jakarta.websocket.Session;
 
-/**
- * Expected Diagnostics are related to validating that the parameters for the 
- * WebSocket methods are of valid types for onOpen (diagnostic code: OnOpenChangeInvalidParam) 
- * and onClose (diagnostic code: OnCloseChangeInvalidParam).
- * See issues #247 (onOpen) and #248 (onClose)
- */
-@ServerEndpoint(value = "/infos")
+@ServerEndpoint(value = "/demo/{test}/var/{abcd}")
 public class InvalidParamTypeText {
     @OnOpen
     public void OnOpen(Session session) throws IOException {
@@ -21,7 +23,7 @@ public class InvalidParamTypeText {
     }
     
     @OnMessage
-    public void OnMessage(Session session, String message, boolean parts, ByteBuffer invalid) throws IOException {
+    public void OnMessage(Session session, String message, ByteBuffer invalid) throws IOException {
         System.out.println("Websocket opened: " + session.getId().toString());
     }
     
