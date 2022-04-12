d92e703a30414584a3535f0fd21703e96565ea67
Aviral Saxena <aviral1231@gmail.com>
2022-04-11 23:03:40 -0700

    Fix Syntax

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/core/websocket/WebSocketConstants.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/core/websocket/WebSocketConstants.java
@@ -138,4 +138,4 @@ public class WebSocketConstants {
     public static final String REGEX_RELATIVE_PATHS = ".*\\/\\.{0,2}\\/.*";
     // Check that a URI string is a valid level 1 variable (wrapped in curly brackets): alpha-numeric characters, dash, or a percent encoded character
     public static final String REGEX_URI_VARIABLE = "\\{(\\w|-|%20|%21|%23|%24|%25|%26|%27|%28|%29|%2A|%2B|%2C|%2F|%3A|%3B|%3D|%3F|%40|%5B|%5D)+\\}";
-
+}
