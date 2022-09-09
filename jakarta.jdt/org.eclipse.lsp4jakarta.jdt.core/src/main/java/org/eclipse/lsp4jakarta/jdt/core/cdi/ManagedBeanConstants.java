c1d494ca09a791c48d37867f60fea6b3e2eefbba
bensonnw <benson.ning@gmail.com>
2022-09-09 10:15:49 -0400

    Validate cid annotations
    Signed-off-by: bensonnw <benson.ning@gmail.com>

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/core/cdi/ManagedBeanConstants.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/core/cdi/ManagedBeanConstants.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2021 IBM Corporation.
+* Copyright (c) 2021, 2022 IBM Corporation.
 *
 * This program and the accompanying materials are made available under the
 * terms of the Eclipse Public License v. 2.0 which is available at
@@ -17,16 +17,20 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.eclipse.lsp4j.DiagnosticSeverity;
-
 public class ManagedBeanConstants {
     /* Annotation Constants */
     public static final String PRODUCES = "Produces";
+    public static final String PRODUCES_FQ_NAME = "jakarta.enterprise.inject.Produces";
     public static final String INJECT = "Inject";
+    public static final String INJECT_FQ_NAME = "jakarta.inject.Inject";
     public static final String DISPOSES = "Disposes";
+    public static final String DISPOSES_FQ_NAME = "jakarta.enterprise.inject.Disposes";
     public static final String OBSERVES = "Observes";
+    public static final String OBSERVES_FQ_NAME = "jakarta.enterprise.event.Observes";
     public static final String OBSERVES_ASYNC = "ObservesAsync";
+    public static final String OBSERVES_ASYNC_FQ_NAME = "jakarta.enterprise.event.ObservesAsync";
     public static final String DEPENDENT = "Dependent";
+    public static final String DEPENDENT_FQ_NAME = "jakarta.enterprise.context.Dependent";
     
     
     public static final String DIAGNOSTIC_SOURCE = "jakarta-cdi";
@@ -44,12 +48,17 @@ public class ManagedBeanConstants {
     public static final String DIAGNOSTIC_CODE_REDUNDANT_DISPOSES = "RemoveExtraDisposes";
     
     public static final Set<String> INVALID_INJECT_PARAMS = new HashSet<String>(Arrays.asList(DISPOSES, OBSERVES, OBSERVES_ASYNC));
+    public static final String[] INVALID_INJECT_PARAMS_FQ = { DISPOSES_FQ_NAME, OBSERVES_FQ_NAME,
+            OBSERVES_ASYNC_FQ_NAME };
     
-    public static final DiagnosticSeverity SEVERITY = DiagnosticSeverity.Error;
-
     // List can be found in the cdi doc here:
     // https://jakarta.ee/specifications/cdi/3.0/jakarta-cdi-spec-3.0.html#bean_defining_annotations
     public static final Set<String> SCOPES = new HashSet<String>(
             Arrays.asList("Dependent", "ApplicationScoped", "ConversationScoped", "RequestScoped", "SessionScoped",
                     "NormalScope", "Interceptor", "Decorator", "Stereotype"));
+    public static final Set<String> SCOPE_FQ_NAMES = new HashSet<String>(
+            Arrays.asList(DEPENDENT_FQ_NAME, "jakarta.enterprise.context.ApplicationScoped",
+                    "jakarta.enterprise.context.ConversationScoped", "jakarta.enterprise.context.RequestScoped",
+                    "jakarta.enterprise.context.SessionScoped", "jakarta.enterprise.context.NormalScope",
+                    "jakarta.Interceptor", "jakarta.Decorator", "jakarta.enterprise.inject.Stereotype"));
 }
