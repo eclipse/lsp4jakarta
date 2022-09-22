58cf6793dcad9ec5cc28734f54dd67243efcd934
bensonnw <benson.ning@gmail.com>
2022-09-22 09:31:09 -0400

    Validate "Inject" annotation
    Signed-off-by: bensonnw <benson.ning@gmail.com>

--- jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/core/di/DependencyInjectionConstants.java
+++ jakarta.jdt/org.eclipse.lsp4jakarta.jdt.core/src/main/java/org/eclipse/lsp4jakarta/jdt/core/di/DependencyInjectionConstants.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2021 IBM Corporation.
+* Copyright (c) 2021, 2022 IBM Corporation.
 *
 * This program and the accompanying materials are made available under the
 * terms of the Eclipse Public License v. 2.0 which is available at
@@ -10,10 +10,7 @@
 * Contributors:
 *     Himanshu Chotwani
 *******************************************************************************/
-
 package org.eclipse.lsp4jakarta.jdt.core.di;
-
-import org.eclipse.lsp4j.DiagnosticSeverity;
 
 public class DependencyInjectionConstants {
 
@@ -22,6 +19,7 @@ public class DependencyInjectionConstants {
 
     public static final String PRODUCES = "Produces";
     public static final String INJECT = "Inject";
+    public static final String INJECT_FQ_NAME = "jakarta.inject.Inject";
     public static final String QUALIFIER = "Qualifier";
     public static final String NAMED = "Named";
 
@@ -34,6 +32,4 @@ public class DependencyInjectionConstants {
     public static final String DIAGNOSTIC_CODE_INJECT_ABSTRACT = "RemoveInjectOrAbstract";
     public static final String DIAGNOSTIC_CODE_INJECT_STATIC = "RemoveInjectOrStatic";
     public static final String DIAGNOSTIC_CODE_INJECT_GENERIC = "RemoveInjectForGeneric";
-
-    public static final DiagnosticSeverity SEVERITY = DiagnosticSeverity.Error;
 }
