cd43bb4a7ddad93174b9230f19677d1cf21008e4
Kathryn Kodama <kathryn.s.kodama@gmail.com>
2022-12-05 13:14:27 -0500

    Remove leftover unused hover code
    Signed-off-by: Kathryn Kodama <kathryn.s.kodama@gmail.com>

--- jakarta.ls/src/main/java/org/eclipse/lsp4jakarta/JakartaTextDocumentService.java
+++ jakarta.ls/src/main/java/org/eclipse/lsp4jakarta/JakartaTextDocumentService.java
@@ -143,12 +143,6 @@ public class JakartaTextDocumentService implements TextDocumentService {
     }
 
     @Override
-    public CompletableFuture<Hover> hover(HoverParams params) {
-        LOGGER.info("received textDocument/hover request");
-        return CompletableFuture.completedFuture(null);
-    }
-
-    @Override
     public CompletableFuture<List<Either<Command, CodeAction>>> codeAction(CodeActionParams params) {
         // Prepare the JakartaJavaCodeActionParams
         JakartaJavaCodeActionParams jakartaCodeActionParams = new JakartaJavaCodeActionParams(params);
