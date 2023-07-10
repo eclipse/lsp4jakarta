76d0c218fa02834d114b73811bfef845f410fa8c
VAISAKH T <VAISAKH.T@ibm.com>
2023-07-10 13:24:19 +0530

    Update JakartaSnippetRegistryTest.java

--- jakarta.ls/src/test/java/org/eclipse/lsp4jakarta/snippets/JakartaSnippetRegistryTest.java
+++ jakarta.ls/src/test/java/org/eclipse/lsp4jakarta/snippets/JakartaSnippetRegistryTest.java
@@ -37,11 +37,90 @@ public class JakartaSnippetRegistryTest {
 	 */
 	@Test
 	public void persistenceSnippetsTest() {
-        Optional<Snippet> persistenceSnippet = findByPrefix("persist_context", registry);
-		assertTrue("Tests has persist_context Java snippet", persistenceSnippet.isPresent());
+        Optional<Snippet> persistcontextSnippet = findByPrefix("persist_context", registry);
+		assertTrue("Tests has persist_context Java snippet", persistcontextSnippet.isPresent());
 		
+		Optional<Snippet> persistcontextextendedSnippet = findByPrefix("persist_context_extended", registry);
+		assertTrue("Tests has persist_context_extended Java snippet", persistcontextextendedSnippet.isPresent());
+		
+		Optional<Snippet> persistcontextextendedunsyncSnippet = findByPrefix("persist_context_extended_unsync", registry);
+		assertTrue("Tests has persist_context_extended_unsync Java snippet", persistcontextextendedunsyncSnippet.isPresent());
+		
+		Optional<Snippet> persistentitySnippet = findByPrefix("persist_entity", registry);
+		assertTrue("Tests has persist_entity Java snippet", persistentitySnippet.isPresent());
 		
 	}
+	
+	
+	/**
+	 * Jakarta RESTful Web Services snippets - rest_class, rest_get
+	 * rest_post, rest_put, rest_delete, rest_head  
+	 */
+	@Test
+	public void RESTfulWebServicesSnippetsTest() {
+        Optional<Snippet> restclassSnippet = findByPrefix("rest_class", registry);
+		assertTrue("Tests has rest_class Java snippet", restclassSnippet.isPresent());
+		
+		
+		Optional<Snippet> restgetSnippet = findByPrefix("rest_get", registry);
+		assertTrue("Tests has rest_get Java snippet", restgetSnippet.isPresent());
+		
+		Optional<Snippet> restpostSnippet = findByPrefix("rest_post", registry);
+		assertTrue("Tests has rest_post Java snippet", restpostSnippet.isPresent());
+		
+		Optional<Snippet> restputSnippet = findByPrefix("rest_put", registry);
+		assertTrue("Tests has rest_put Java snippet", restputSnippet.isPresent());
+		
+		Optional<Snippet> restdeleteSnippet = findByPrefix("rest_delete", registry);
+		assertTrue("Tests has rest_delete Java snippet", restdeleteSnippet.isPresent());
+		
+		Optional<Snippet> restheadSnippet = findByPrefix("rest_head", registry);
+		assertTrue("Tests has rest_head Java snippet", restheadSnippet.isPresent());
+		
+	}
+	
+	
+	
+	/**
+	 * Jakarta Servlet snippets - servlet_generic, servlet_doget
+	 * servlet_dopost, servlet_webfilter
+	 */
+	@Test
+	public void ServletSnippetsTest() {
+        Optional<Snippet> servletgenericSnippet = findByPrefix("servlet_generic", registry);
+		assertTrue("Tests has servlet_generic Java snippet", servletgenericSnippet.isPresent());
+		
+		Optional<Snippet> servletdogetSnippet = findByPrefix("servlet_doget", registry);
+		assertTrue("Tests has servlet_doget Java snippet", servletdogetSnippet.isPresent());
+		
+		Optional<Snippet> servletdopostSnippet = findByPrefix("servlet_dopost", registry);
+		assertTrue("Tests has servlet_dopost Java snippet", servletdopostSnippet.isPresent());
+		
+		Optional<Snippet> servletwebfilterSnippet = findByPrefix("servlet_webfilter", registry);
+		assertTrue("Tests has servlet_webfilter Java snippet", servletwebfilterSnippet.isPresent());
+	}
+	
+	
+	/**
+	 * Jakarta Transactions snippets -  tx_user_inject,
+	 * tx_user_jndi, @ Transactional
+	 */
+	@Test
+	public void TransactionsSnippetsTest() {
+        Optional<Snippet> txuserinjectSnippet = findByPrefix("tx_user_inject", registry);
+		assertTrue("Tests has tx_user_inject Java snippet", txuserinjectSnippet.isPresent());
+		
+		Optional<Snippet> txuserjndiSnippet = findByPrefix("tx_user_jndi", registry);
+		assertTrue("Tests has tx_user_jndi Java snippet", txuserjndiSnippet.isPresent());
+		
+		Optional<Snippet> TransactionalSnippet = findByPrefix("@Transactional", registry);
+		assertTrue("Tests has @Transactional Java snippet", TransactionalSnippet.isPresent());
+	}
+
+
+	
+	
+	
 
 	
 	// Verify whether the snippet is present in the registry.
